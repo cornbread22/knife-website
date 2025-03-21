c329478afb493a2168e019f77b9912b72256c35b
Asher Leffler <asher.leffler@gmail.com>
2025-03-21 10:27:35 -0400

    Organize files and update paths

--- src/js/function.js
+++ src/js/function.js
@@ -1,6 +1,6 @@
 document.getElementById("carbonblackened").addEventListener("click", function () {
   var img = document.getElementById("imageElementId"); // Replace with the actual ID of the image element
-  img.src = "./images/carbonblackened.jpg";
+  img.src = "../../images/carbonblackened.jpg";
   img.addEventListener("click", function () {
     img.classList.toggle("expanded");
   });
